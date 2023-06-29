e75c593d3a3dd1a589e95d539fc56136d1a6e513
Stampy Sakai <53864221+StampySakai@users.noreply.github.com>
2023-06-29 08:25:18 -0700

    Add project files.

---
+++ Initial OVR and Input Emulator Interface/main.cpp
@@ -0,0 +1,34 @@
+// Initial OVR and Input Emulator Interface.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <thread>
+#include <openvr.h>
+#include <vrinputemulator.h>
+#include <vector>
+#define GLM_FORCE_SWIZZLE
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp> 
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <csignal>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
